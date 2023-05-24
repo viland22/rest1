9aa0bad68d9decfc6018282c7470110d4f592f3d
Rodina Vilandy <vilandy.rodina@gmail.com>
2023-05-24 10:18:50 +0700

    uncommect mysql

--- index.js
+++ index.js
@@ -5,56 +5,56 @@ const app = express();
 const data = require('./data');
 var cors = require('cors')
 
-// // Membuat koneksi ke MySQL
-// const connection = mysql.createConnection({
-//     host: 'bmqzdqbo211z6nkoejlc-mysql.services.clever-cloud.com', //'localhost',
-//     user: 'uavivtxexz8xonoy', //'root',
-//     password: '815mQNJJgJ1n9GShcQ4O', //'',
-//     database: 'bmqzdqbo211z6nkoejlc' //'test'
-//   });
+// Membuat koneksi ke MySQL
+const connection = mysql.createConnection({
+    host: 'bmqzdqbo211z6nkoejlc-mysql.services.clever-cloud.com', //'localhost',
+    user: 'uavivtxexz8xonoy', //'root',
+    password: '815mQNJJgJ1n9GShcQ4O', //'',
+    database: 'bmqzdqbo211z6nkoejlc' //'test'
+  });
 
-// // Menghubungkan ke MySQL saat server Express.js dijalankan
-// connection.connect((error) => {
-//     if (error) {
-//       console.error('Error connecting to MySQL database:', error);
-//     } else {
-//       console.log('Connected to MySQL database');
-//     }
-//   });
+// Menghubungkan ke MySQL saat server Express.js dijalankan
+connection.connect((error) => {
+    if (error) {
+      console.error('Error connecting to MySQL database:', error);
+    } else {
+      console.log('Connected to MySQL database');
+    }
+  });
 
-// app.get('/lagulist', (req, res) => {
-//     connection.query('SELECT * FROM vw_lagulist', (error, results) => {
-//         if (error) {
-//         console.error('Error executing MySQL query:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//         } else {
-//         res.json(results);
-//         }
-//     });
-// }); 
+app.get('/lagulist', (req, res) => {
+    connection.query('SELECT * FROM vw_lagulist', (error, results) => {
+        if (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+        res.json(results);
+        }
+    });
+}); 
 
-// app.get('/bandlist', (req, res) => {
-//     connection.query('SELECT * FROM vw_bandlist', (error, results) => {
-//         if (error) {
-//         console.error('Error executing MySQL query:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//         } else {
-//         res.json(results);
-//         }
-//     });
-// }); 
+app.get('/bandlist', (req, res) => {
+    connection.query('SELECT * FROM vw_bandlist', (error, results) => {
+        if (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+        res.json(results);
+        }
+    });
+}); 
 
-// app.get('/lagulist/:id', (req, res) => {
-//     const NotId = req.params.id;
-//     connection.query('SELECT * FROM vw_lagulist where id = ?',[NotId], (error, results) => {
-//         if (error) {
-//         console.error('Error executing MySQL query:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//         } else {
-//         res.json(results);
-//         }
-//     });
-// }); 
+app.get('/lagulist/:id', (req, res) => {
+    const NotId = req.params.id;
+    connection.query('SELECT * FROM vw_lagulist where id = ?',[NotId], (error, results) => {
+        if (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+        res.json(results);
+        }
+    });
+}); 
 
 app.use(cors());
 
