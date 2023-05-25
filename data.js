c979efea43a21581ce092a959059043609900f8e
Rodina Vilandy <vilandy.rodina@gmail.com>
2023-05-25 18:19:33 +0700

    comment mysql

--- data.js
+++ data.js
@@ -1,40 +1,16 @@
 // data.js
 const data = [
-    {
-        id:1,
-        band: "band 1",
-        lagu: "lagu 1",
-        isi: "lagu1.txt",
-        tanggal:"2023-05-22"
-    },
-    {
-        id:2,
-        band: "band 2",
-        lagu: "lagu 2",
-        isi: "lagu2.txt",
-        tanggal:"2023-05-22"
-    },
-    {
-        id:3,
-        band: "band 1",
-        lagu: "lagu 1",
-        isi: "lagu1.txt",
-        tanggal:"2023-05-22"
-    },
-    {
-        id:4,
-        band: "band 2",
-        lagu: "lagu 2",
-        isi: "lagu2.txt",
-        tanggal:"2023-05-22"
-    },
-    {
-        id:5,
-        band: "Peterpan",
-        lagu: "aadm",
-        isi: "lagu2.txt",
-        tanggal:"2023-05-22"
-    },
+    { id:1, artist:'Melly Goeslaw', lagu: 'Bunda', text: 'bunda.txt', tanggal: '5/25/2023 18:17' },
+    { id:2, artist:'Kahitna', lagu: 'Cantik', text: 'cantik.txt', tanggal: '5/25/2023 18:17' },
+    { id:3, artist:'Rossa', lagu: 'Hati yang kau sakiti', text: 'hati yang kau sakiti.txt', tanggal: '5/25/2023 18:17' },
+    { id:4, artist:'Nidji', lagu: 'Laskar pelangi', text: 'laskar pelangi.txt', tanggal: '5/25/2023 18:17' },
+    { id:5, artist:'Sherina', lagu: 'Lihat lebih dekat', text: 'lihat lebih dekat.txt', tanggal: '5/25/2023 18:17' },
+    { id:6, artist:'Kahitna', lagu: 'Mantan terindah', text: 'mantan terindah.txt', tanggal: '5/25/2023 18:17' },
+    { id:7, artist:'Kotak', lagu: 'Pelan pelan saja', text: 'pelan pelan saja.txt', tanggal: '5/25/2023 18:17' },
+    { id:8, artist:'Ungu', lagu: 'Saat bahagia', text: 'saat bahagia.txt', tanggal: '5/25/2023 18:17' },
+    { id:9, artist:'Ade Govinda', lagu: 'Tanpa batas waktu', text: 'tanpa batas waktu.txt', tanggal: '5/25/2023 18:17' },
+    { id:10, artist:'Tahta', lagu: 'Tempat paling indah', text: 'tempat paling indah.txt', tanggal: '5/25/2023 18:17' }
+
     ];
   
   module.exports = data;
--- index.js
+++ index.js
@@ -7,72 +7,72 @@ var cors = require('cors')
 
 app.use(cors());
 
-// Membuat koneksi ke MySQL
-const connection = mysql.createConnection({
-    host: 'bmqzdqbo211z6nkoejlc-mysql.services.clever-cloud.com', //'localhost',
-    user: 'uavivtxexz8xonoy', //'root',
-    password: '815mQNJJgJ1n9GShcQ4O', //'',
-    database: 'bmqzdqbo211z6nkoejlc' //'test'
-  });
+// // Membuat koneksi ke MySQL
+// const connection = mysql.createConnection({
+//     host: 'bmqzdqbo211z6nkoejlc-mysql.services.clever-cloud.com', //'localhost',
+//     user: 'uavivtxexz8xonoy', //'root',
+//     password: '815mQNJJgJ1n9GShcQ4O', //'',
+//     database: 'bmqzdqbo211z6nkoejlc' //'test'
+//   });
 
-// Menghubungkan ke MySQL saat server Express.js dijalankan
-connection.connect((error) => {
-    if (error) {
-      console.error('Error connecting to MySQL database:', error);
-    } else {
-      console.log('Connected to MySQL database');
-    }
-  });
+// // Menghubungkan ke MySQL saat server Express.js dijalankan
+// connection.connect((error) => {
+//     if (error) {
+//       console.error('Error connecting to MySQL database:', error);
+//     } else {
+//       console.log('Connected to MySQL database');
+//     }
+//   });
 
-app.get('/lagulist', (req, res) => {
-    connection.query('SELECT * FROM vw_lagulist', (error, results) => {
-        if (error) {
-        console.error('Error executing MySQL query:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-        res.json(results);
-        }
-    });
-}); 
+// app.get('/lagulist', (req, res) => {
+//     connection.query('SELECT * FROM vw_lagulist', (error, results) => {
+//         if (error) {
+//         console.error('Error executing MySQL query:', error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//         } else {
+//         res.json(results);
+//         }
+//     });
+// }); 
 
-app.get('/bandlist', (req, res) => {
-    connection.query('SELECT * FROM vw_bandlist', (error, results) => {
-        if (error) {
-        console.error('Error executing MySQL query:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-        res.json(results);
-        }
-    });
-}); 
+// app.get('/bandlist', (req, res) => {
+//     connection.query('SELECT * FROM vw_bandlist', (error, results) => {
+//         if (error) {
+//         console.error('Error executing MySQL query:', error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//         } else {
+//         res.json(results);
+//         }
+//     });
+// }); 
 
-app.get('/lagulist/:id', (req, res) => {
-    const NotId = req.params.id;
-    connection.query('SELECT * FROM vw_lagulist where id = ?',[NotId], (error, results) => {
-        if (error) {
-        console.error('Error executing MySQL query:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-        res.json(results);
-        }
-    });
-}); 
+// app.get('/lagulist/:id', (req, res) => {
+//     const NotId = req.params.id;
+//     connection.query('SELECT * FROM vw_lagulist where id = ?',[NotId], (error, results) => {
+//         if (error) {
+//         console.error('Error executing MySQL query:', error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//         } else {
+//         res.json(results);
+//         }
+//     });
+// }); 
 
 
 
-app.get('/lagudata', (req, res) => {
+app.get('/daftarlagu', (req, res) => {
     //console.log(data);
     res.json(data); 
 });
 
-app.get('/user', (req, res) => {
-  const user = {
-    name: 'John Doe',
-    profileImage: '/uploads/profile.jpg'
-  };
+// app.get('/user', (req, res) => {
+//   const user = {
+//     name: 'John Doe',
+//     profileImage: '/uploads/profile.jpg'
+//   };
 
-  res.json(user);
-});
+//   res.json(user);
+// });
 
 // Menyajikan gambar dari folder 'uploads'
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
